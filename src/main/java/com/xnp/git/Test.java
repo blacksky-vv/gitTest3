c363351eceb597fc13ef3ebd4ea6eda6bdbdcb5a
xnp <1668440768@qq.com>
2022-04-25 18:46:47 +0800

    master commit2

--- src/main/java/com/xnp/git/Test.java
+++ src/main/java/com/xnp/git/Test.java
@@ -6,5 +6,6 @@ public class Test {
         System.out.println("git hello2");
         System.out.println("git hello3");
         System.out.println("git helloXX");
+        System.out.println("git helloXX masterTest");
     }
 }
