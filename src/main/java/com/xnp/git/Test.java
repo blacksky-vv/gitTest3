140d246b9e99a17dc44cd2402288c261451a9e08
xnp <1668440768@qq.com>
2022-04-25 18:20:48 +0800

    xnp commit1

--- src/main/java/com/xnp/git/Test.java
+++ src/main/java/com/xnp/git/Test.java
@@ -5,5 +5,6 @@ public class Test {
         System.out.println("git hello");
         System.out.println("git hello2");
         System.out.println("git hello3");
+        System.out.println("git helloXX");
     }
 }
